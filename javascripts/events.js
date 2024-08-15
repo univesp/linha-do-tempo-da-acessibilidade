7ecbfb04186323d6f86f63cf1d31cdeff2edcd3b
Caique Silva <caique_silva94@hotmail.com>
2024-08-15 11:49:56 -0300

    mobile responsive / add animate

--- javascripts/events.js
+++ javascripts/events.js
@@ -29,9 +29,61 @@ document.addEventListener("DOMContentLoaded", function() {
       const posicaoSection = section.getBoundingClientRect();
       const alturaTotal = section.scrollHeight;
       line.style.height = `${alturaTotal}px`;
-      line.style.top = `${posicaoSection.top + window.scrollY}px`;
+      // line.style.top = `${posicaoSection.top + window.scrollY}px`;
+
+
+  }
+
+  console.log("_____________________________________________________");
+  console.log(section.children.length);
+  console.log(section.children[0]);
+  console.log(section.children[1].children[1]);
+  console.log(section.children[2]);
+  console.log(section.children[3]);
+  
+  var n = 1;
+
+  for (let index = 1; index < section.children.length; index++) {
+    if(index % 2 == 0){
+      section.children[index].children[1].classList.add("animate__animated");
+      // section.children[index].children[1].classList.add("animate__backInRight");
+      section.children[index].children[1].classList.add("animate__backInLeft");
+      section.children[index].children[1].classList.add("paused"); 
+    } else {
+      section.children[index].children[1].classList.add("animate__animated");
+      // section.children[index].children[1].classList.add("animate__backInLeft");
+      section.children[index].children[1].classList.add("animate__backInRight");
+      section.children[index].children[1].classList.add("paused"); 
+    }
+
   }
 
   ajustarLinha();
-  window.addEventListener("resize", ajustarLinha);
+    window.addEventListener("resize", ajustarLinha);
+
+    window.addEventListener('scroll', function() {
+      // Verifica se o usuário rolou para baixo
+      if (window.scrollY > 0) {
+          // Atualiza a página
+          // location.reload();
+          
+          // console.log(section.children[1].children[1].getBoundingClientRect());
+          var rect = section.children[n].children[1].getBoundingClientRect();
+          if ((rect.top+100) <= window.innerHeight) {
+            console.log("deu certo" + n);
+            n += 1;
+            if(n % 2 == 0){
+              console.log("par");
+              section.children[n-1].children[1].classList.remove("paused");
+            } else {
+              console.log("impar");
+              section.children[n-1].children[1].classList.remove("paused");
+
+              // section.children[n-1].children[1].classList.add("animate__animated");
+              // section.children[n-1].children[1].classList.add("animate__rubberBand");
+            }
+        }
+
+      }
+  });
 });
