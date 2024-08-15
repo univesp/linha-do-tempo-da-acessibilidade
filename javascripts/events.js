811467745c0bdf110c1cd5e1b09c5de5771d30e1
Caique Silva <caique_silva94@hotmail.com>
2024-08-15 14:12:43 -0300

    ajuste animação no celular

--- javascripts/events.js
+++ javascripts/events.js
@@ -34,16 +34,19 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 
   var n = 1;
+  var teste = document.getElementById("teste");
 
   for (let index = 1; index < section.children.length; index++) {
     if(index % 2 == 0){
       section.children[index].children[1].classList.add("animate__animated");
-      section.children[index].children[1].classList.add("animate__backInLeft");
-      section.children[index].children[1].classList.add("paused"); 
+      // section.children[index].children[1].classList.add("animate__backInLeft");
+      // section.children[index].children[1].classList.add("paused"); 
+      // section.children[index].children[1].classList.add("display-none"); 
     } else {
       section.children[index].children[1].classList.add("animate__animated");
-      section.children[index].children[1].classList.add("animate__backInRight");
-      section.children[index].children[1].classList.add("paused"); 
+      // section.children[index].children[1].classList.add("animate__backInRight");
+      // section.children[index].children[1].classList.add("paused"); 
+      // section.children[index].children[1].classList.add("display-none"); 
     }
 
   }
@@ -53,14 +56,24 @@ document.addEventListener("DOMContentLoaded", function() {
 
     window.addEventListener('scroll', function() {
       // Verifica se o usuário rolou para baixo
+      console.log(window.scrollY);
+      console.log(window.innerHeight);
       if (window.scrollY > 0) {
           var rect = section.children[n].children[1].getBoundingClientRect();
-          if ((rect.top+0) <= window.innerHeight) {
+          console.log(rect)
+          if (rect.y <= window.innerHeight) {
             n += 1;
+            teste.innerHTML = (rect.y) + " : " + this.window.innerHeight;
+            console.log(rect.top)
+            console.log(rect.bottom)
             if(n % 2 == 0){
               section.children[n-1].children[1].classList.remove("paused");
+              section.children[n-1].children[1].classList.remove("display-none");
+              section.children[n-1].children[1].classList.add("animate__backInRight");
             } else {
               section.children[n-1].children[1].classList.remove("paused");
+              section.children[n-1].children[1].classList.remove("display-none");
+              section.children[n-1].children[1].classList.add("animate__backInLeft");
 
             }
         }
