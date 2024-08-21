af56abf1e1abd5120727f930f45d17d856cd63f0
Caique Silva <caique_silva94@hotmail.com>
2024-08-21 08:30:29 -0300

    alterações js e css

--- javascripts/events.js
+++ javascripts/events.js
@@ -18,6 +18,49 @@ $(document).ready(function(){
   //////////////////////////////////////////////////////////////////
 
   // Seu código abaixo
+  $(function() {
+    var w=$(window);
+     var sliderContainer = $("#menu-slider-container");
+     var sliderUl = $("#menu-slider");
+     var sliderLi = sliderUl.children("li");
+     var sliderNext = $("#menu-slider-next");
+     var sliderPrev = $("#menu-slider-prev");
+    var sliderLiWidth = sliderLi.eq(0).width();
+     sliderUl.width(sliderLi.length * sliderLiWidth);
+     
+     if (sliderUl.width() > sliderContainer.width()) {
+       sliderNext.fadeIn();
+     }
+     
+    sliderNext.on("click", function() {
+         var x = parseInt(sliderUl.css("marginLeft"));
+         var ulWidth = sliderUl.width();
+        if (ulWidth + x >= sliderContainer.width()) {
+           x -= sliderLiWidth * 3;
+           if (ulWidth + x < sliderContainer.width()) { x = sliderContainer.width() - ulWidth - 10; }
+           sliderUl.stop().animate({ marginLeft: x }, 800);
+          sliderPrev.fadeIn();
+          console.log(parseInt(sliderUl.css("marginLeft"))*-1, ulWidth - sliderContainer.width());
+         }
+      if (parseInt(sliderUl.css("marginLeft"))*-1 >= ulWidth - sliderContainer.width()*2) {
+            sliderNext.fadeOut();
+          }
+       });
+        
+     sliderPrev.on("click", function() {
+         var x = parseInt(sliderUl.css("marginLeft"));
+         var ulWidth = sliderUl.width();
+        if (x <= 0) { 
+           x += sliderLiWidth * 3;
+           if (x > 0) { x = 0; }
+           sliderUl.stop().animate({ marginLeft: x }, 800);
+          sliderNext.fadeIn();
+          if (x == 0) {
+            sliderPrev.fadeOut();
+          }
+         }
+       });
+   });
 
 })
 
